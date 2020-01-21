4c8d5baa23483792db46658937d889b84b0de8d8
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-01-21 13:20:40 -0300

    CRUD

--- modulos/modulo01/index.js
+++ modulos/modulo01/index.js
@@ -2,16 +2,45 @@ const express = require('express');
 
 const server = express();
 
-// Query params = ?id=1
-// Route params = /users/1
-// Request body = { "name": "Anderson", "email": "anderson@email.com" }
+server.use(express.json());
+
+// CRUD - Create, Read, Update, Delete
 
 const users = ['Anderson', 'Alvina', 'Andressa'];
+
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
 
 server.get('/users/:index', (req, res) => {
   const { index } = req.params;
 
   return res.json(users[index]);
+});
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.json(users);
 });
 
 server.listen(3000);
