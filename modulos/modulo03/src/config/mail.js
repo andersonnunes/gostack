168a6afb5e0eacd3dd5a79cbc63864cd802e9d20
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-01-26 19:04:25 -0300

    Configurando Nodemailer

---
+++ modulos/modulo03/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'f505a754a2e9b7',
+    pass: '77bed0c6d9add0',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
