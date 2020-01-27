ed74b1d9363b2054427379ff30576fa0d5cd00bb
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-01-26 22:15:40 -0300

    Variáveis ambiente

--- modulos/modulo03/src/config/mail.js
+++ modulos/modulo03/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'f505a754a2e9b7',
-    pass: '77bed0c6d9add0',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
