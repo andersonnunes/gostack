31617e10bfdf3962c99112956638da3822d536ca
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-04-16 16:15:31 -0300

    Validando credenciais

--- aulas/iniciando-backend/src/services/CreateUserService.ts
+++ aulas/iniciando-backend/src/services/CreateUserService.ts
@@ -10,7 +10,7 @@ interface Request {
 }
 
 class CreateUserService {
-  async execute({ name, email, password }: Request): Promise<User> {
+  public async execute({ name, email, password }: Request): Promise<User> {
     const usersRepository = getRepository(User);
 
     const checkUserExists = await usersRepository.findOne({
