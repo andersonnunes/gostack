2e5147812cfaf1c13e015693539195771cb48721
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-04-16 15:17:12 -0300

    Relacionamento nos models

--- aulas/iniciando-backend/src/database/migrations/1587054280171-CreateAppointment.ts
+++ aulas/iniciando-backend/src/database/migrations/1587054280171-CreateAppointment.ts
@@ -9,7 +9,7 @@ export default class CreateAppointment1587054280171
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
