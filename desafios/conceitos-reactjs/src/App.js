1d3f2137afb9b4f66ea696864ddbb3ae30c3cbcf
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-04-21 03:04:51 -0300

    Desafio: Conceitos do ReactJS

---
+++ desafios/conceitos-reactjs/src/App.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+
+import api from './services/api';
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories')
+      .then(response => {
+        setRepositories(response.data);
+      });
+    
+  }, [])
+
+  async function handleAddRepository() {
+    const response = await api.post('/repositories', {
+      url: "https://github.com/Rocketseat/unform",
+      title: "Unform",
+      techs: ["React", "ReactNative", "TypeScript", "ContextApi"]
+    });
+
+    setRepositories([...repositories, response.data]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`/repositories/${id}`);
+
+    setRepositories(repositories.filter(repository => repository.id !== id));
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        { repositories && repositories.map(repository => (
+          <li key={repository.id}>
+            { repository.title }
+
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        )) }
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
