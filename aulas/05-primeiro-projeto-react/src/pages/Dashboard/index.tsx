bd6cc32807a35195dd9dff5ff9db14915365ab4e
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-04-21 20:58:47 -0300

    Salvando no Storage

--- aulas/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ aulas/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, FormEvent, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -7,20 +7,37 @@ import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Error, Repositories } from './styles';
 
+interface Repository {
+  id: number;
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
 
-  interface Repository {
-    id: number;
-    full_name: string;
-    description: string;
-    owner: {
-      login: string;
-      avatar_url: string;
-    };
-  }
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
 
   async function handleAddRepository(
     event: FormEvent<HTMLFormElement>,
