62a4a8325b263d9a9d48fc9ddd56c4f83b335585
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-04-21 02:47:17 -0300

    Listando Projetos da API

--- aulas/conceitos-dev/frontend/src/App.js
+++ aulas/conceitos-dev/frontend/src/App.js
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./components/Header";
+
+import api from "./services/api";
 
 import "./App.css";
 
 import backgroundImage from "./assets/background.jpeg";
 
 function App() {
-  const [projects, setProjects] = useState([
-    "Desenvolvimento de app",
-    "Front-end web",
-  ]);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get("projects").then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
 
   function handleAddProject() {
     setProjects([...projects, `Novo projeto ${Date.now()}`]);
@@ -22,7 +27,7 @@ function App() {
         <img width={300} src={backgroundImage} alt="Background" />
         <ul>
           {projects.map((project) => (
-            <li key={project}>{project}</li>
+            <li key={project.id}>{project.title}</li>
           ))}
         </ul>
 
