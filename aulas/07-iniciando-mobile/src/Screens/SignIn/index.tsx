2d3528c824753760b6c0b3f188605651e3354f93
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-04-25 17:15:59 -0300

    Tela de autenticação

--- aulas/07-iniciando-mobile/src/Screens/SignIn/index.tsx
+++ aulas/07-iniciando-mobile/src/Screens/SignIn/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, ScrollView, KeyboardAvoidingView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 import logoImg from '../../assets/logo.png';
 
@@ -10,13 +18,32 @@ import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu logon</Title>
-      <Input name="email" placeholder="E-mail" icon="mail" />
-      <Input name="password" placeholder="Senha" icon="lock" />
-      <Button onPress={() => {}}>Entrar</Button>
-    </Container>
+    <>
+      <KeyboardAvoidingView style={{ flex: 1 }} enabled>
+        <ScrollView keyboardShouldPersistTaps="handled">
+          <Container>
+            <Image source={logoImg} />
+
+            <Title>Faça seu logon</Title>
+
+            <Input name="email" placeholder="E-mail" icon="mail" />
+
+            <Input name="password" placeholder="Senha" icon="lock" />
+
+            <Button onPress={() => {}}>Entrar</Button>
+
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+
+        <CreateAccountButton onPress={() => {}}>
+          <Icon name="log-in" size={20} color="#ff9000" />
+          <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+        </CreateAccountButton>
+      </KeyboardAvoidingView>
+    </>
   );
 };
 export default SignIn;
