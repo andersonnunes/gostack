1b3cd40acbfccc30cc2ff7efb996f87a9a0d3d60
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-04-25 16:13:22 -0300

    Input & Button

--- aulas/07-iniciando-mobile/src/Screens/SignIn/index.tsx
+++ aulas/07-iniciando-mobile/src/Screens/SignIn/index.tsx
@@ -5,11 +5,17 @@ import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+      <Input name="email" placeholder="E-mail" icon="mail" />
+      <Input name="password" placeholder="Senha" icon="lock" />
+      <Button onPress={() => {}}>Entrar</Button>
     </Container>
   );
 };
