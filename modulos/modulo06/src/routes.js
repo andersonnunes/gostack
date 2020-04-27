7a8b7635bb64e78b6f6624a3a9cc636b914ca45f
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-04-27 16:36:06 -0300

    React Navigation

---
+++ modulos/modulo06/src/routes.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Main from './screens/Main';
+import User from './screens/User';
+
+const {Navigator, Screen} = createStackNavigator();
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Navigator
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerBackTitleVisible: false,
+          headerStyle: {
+            backgroundColor: '#7159c1',
+          },
+          headerTintColor: '#FFF',
+        }}>
+        <Screen
+          name="Main"
+          component={Main}
+          options={{
+            title: 'Usuários',
+          }}
+        />
+        <Screen name="User" component={User} />
+      </Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
