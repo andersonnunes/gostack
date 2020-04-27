7fa13909b238924b5a720096ae2bde5e07b01313
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-04-27 19:28:31 -0300

    Listando favoritos

--- modulos/modulo06/src/screens/User/index.js
+++ modulos/modulo06/src/screens/User/index.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import {View} from 'react-native';
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import { Container, Header, Avatar, Name, Bio, Stars, Starred, OwnerAvatar, Info,
+  Title, Author } from './styles';
 
 export default class User extends Component {
   state = {
@@ -19,8 +20,33 @@ export default class User extends Component {
   }
 
   render() {
-    const {stars} = this.state;
+    const { route } = this.props;
+    const { stars } = this.state;
 
-    return <View />;
+    const user = route.params.user;
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
   }
 }
