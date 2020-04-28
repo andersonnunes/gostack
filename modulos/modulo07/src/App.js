428169ce16d444a77c78634f7e5a6bd4a53f5834
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-04-27 22:03:40 -0300

    Criando Header

--- modulos/modulo07/src/App.js
+++ modulos/modulo07/src/App.js
@@ -3,12 +3,15 @@ import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 
+import Header from './components/Header';
+
 import Routes from './routes';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes />
         <GlobalStyle />
       </BrowserRouter>
