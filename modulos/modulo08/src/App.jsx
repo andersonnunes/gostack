bab37b8a1fcd5a59d38e209ccf2ccb1425398061
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-04-29 17:33:52 -0300

    Hook useState

--- modulos/modulo08/src/App.jsx
+++ modulos/modulo08/src/App.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [techs, setTechs] = useState(['ReactJS', 'React Native']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTechs([...techs, newTech]);
+    setNewTech('');
+  }
+
   return (
-    <h1>Hello World</h1>
+    <>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        onChange={e => setNewTech(e.target.value)}
+        value={newTech}
+      />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
   );
 }
 
