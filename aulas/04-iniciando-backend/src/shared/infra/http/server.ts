dbc3438c90768a97cc139cf3b19ea849d2750c10
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-05-04 21:31:22 -0300

    Injeção de dependências

--- aulas/04-iniciando-backend/src/shared/infra/http/server.ts
+++ aulas/04-iniciando-backend/src/shared/infra/http/server.ts
@@ -5,7 +5,9 @@ import cors from 'cors';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+
 import '@shared/infra/typeorm';
+import '@shared/container';
 
 import routes from './routes';
 
