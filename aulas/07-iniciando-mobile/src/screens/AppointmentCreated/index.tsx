99af0f32fc7b461a0d83f9f144604a0b30edba36
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-06-12 17:39:59 -0300

    Criando estrutura da tela

--- aulas/07-iniciando-mobile/src/screens/AppointmentCreated/index.tsx
+++ aulas/07-iniciando-mobile/src/screens/AppointmentCreated/index.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+  Container,
+  Title,
+  Description,
+  OKButton,
+  OKButtonText,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
-  return <View />;
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>Terça, dia 14 de março de 2020 às 12:00h</Description>
+
+      <OKButton>
+        <OKButtonText onPress={handleOkPressed}>OK</OKButtonText>
+      </OKButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
